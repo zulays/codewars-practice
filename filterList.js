abdb93734b54163ce1f8ab74451242471b93f54a
Zulay Scottborgh <zscottborgh@gmail.com>
2021-03-03 23:00:57 -0500

    list has been filtered

---
+++ filterList.js
@@ -0,0 +1,11 @@
+// In this kata you will create a function that
+// takes a list of non - negative integers and strings and
+// returns a new list with the strings filtered out.
+
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+
+      return l.filter(str => typeof str !== 'string')
+}
+
+console.log(filter_list([1,2,'a','b']))
