1636238224bc58d58463cb70ad05e7c03fb8d393
Zulay Scottborgh <zscottborgh@gmail.com>
2021-03-15 19:22:33 -0400

    findOdd in progress

--- findOdd.js
+++ findOdd.js
@@ -3,5 +3,21 @@
 // There will always be only one integer that appears an odd number of times.
 
 function findOdd(A) {
-  return 0
+  // arr = []
+
+  for (let i = 0; i < A.length; i++) {
+    let count = 0
+    for (let j = 0; j < A.length; j++) {
+      if (A[i] == A[j]) {
+        count++
+        if (count % 2 != 0) {
+          return A[i]
+        }
+      }
+    }
+  }
+  return -1
 }
+
+console.log(findOdd([20,1,1,2,2,3,3,5,5,4,20,4,5], 5))
+console.log(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5], -1))
