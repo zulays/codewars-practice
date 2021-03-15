2045867534f95aabd70a1560461e8630f4de0e08
Zulay Scottborgh <zscottborgh@gmail.com>
2021-03-15 19:00:59 -0400

    beginning findOdd algorithm

---
+++ findOdd.js
@@ -0,0 +1,7 @@
+// Given an array of integers,
+// find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+function findOdd(A) {
+  return 0
+}
